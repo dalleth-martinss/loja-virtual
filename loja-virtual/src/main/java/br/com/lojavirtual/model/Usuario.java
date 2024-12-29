b652af677f753a3121c464eec65e82c8d6ab331f
dalleth-martinss <essi_dalleth@outlook.com>
2024-12-29 20:11:41 -0300

    create table Usuario

---
+++ loja-virtual/src/main/java/br/com/lojavirtual/model/Usuario.java
@@ -0,0 +1,79 @@
+package br.com.lojavirtual.model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "usuario")
+@SequenceGenerator(name = "seq_usuario", sequenceName = "seq_usuario", allocationSize = 1, initialValue = 1)
+@Getter
+@Setter
+public class Usuario implements UserDetails {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+        @Id
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_usuario")
+        private Long id;
+
+        private String login;
+
+        private String senha;
+
+        @Temporal(TemporalType.DATE)
+        private Date dateAtualSenha;
+
+        @OneToMany(fetch = FetchType.LAZY)
+        @JoinTable(name = "usuario_acesso", uniqueConstraints = @UniqueConstraint(columnNames = {"usuario_id", "acesso_id"},
+                name = "unique_acesso_user"), joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id",
+                table = "usuario", unique = false, foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)),
+                inverseJoinColumns = @JoinColumn(name = "acesso_id", unique = false, referencedColumnName = "id", table = "acesso",
+                foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT ))
+        )
+        private List<Acesso> acessos;
+
+        /*As athorities = são os acessos ou seja ROLE_ADMIN, ROLE_SECRETARIO, ROLE_FINANCEIRO */
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            // Verifica se acessos não é nulo para evitar NullPointerException
+            return acessos != null ? acessos : List.of();
+        }
+
+        @Override
+        public String getPassword() {
+            return this.senha;
+        }
+
+        @Override
+        public String getUsername() {
+            return this.login;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return UserDetails.super.isAccountNonExpired();
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return UserDetails.super.isAccountNonLocked();
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return UserDetails.super.isCredentialsNonExpired();
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return UserDetails.super.isEnabled();
+        }
+}
