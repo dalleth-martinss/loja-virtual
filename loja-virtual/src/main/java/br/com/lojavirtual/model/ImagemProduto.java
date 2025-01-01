1c304f23d8fc5159218994369a34900d28fd9f75
dalleth-martinss <essi_dalleth@outlook.com>
2025-01-01 15:10:25 -0300

    create table imagem produto

---
+++ loja-virtual/src/main/java/br/com/lojavirtual/model/ImagemProduto.java
@@ -0,0 +1,48 @@
+package br.com.lojavirtual.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "imagem_produto")
+@SequenceGenerator(name = "seq_imagem_produto", sequenceName = "seq_imagem_produto", allocationSize = 1, initialValue = 1)
+@Getter
+@Setter
+public class ImagemProduto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_imagem_produto")
+    private Long id;
+
+    @Column(columnDefinition = "text")
+    private String imagemOriginal;
+
+    @Column(columnDefinition = "text")
+    private String imagemMiniatura;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false,
+    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "produto_fk"))
+    private Produto produto;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ImagemProduto that = (ImagemProduto) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
