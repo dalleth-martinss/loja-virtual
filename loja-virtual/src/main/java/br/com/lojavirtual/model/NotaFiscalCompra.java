9bae79263fd98ebcabf4940f14c2f89fd6a23283
dalleth-martinss <essi_dalleth@outlook.com>
2025-01-02 20:47:21 -0300

    create table nota fiscal compra

---
+++ loja-virtual/src/main/java/br/com/lojavirtual/model/NotaFiscalCompra.java
@@ -0,0 +1,60 @@
+package br.com.lojavirtual.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "nota_fiscal_compra")
+@SequenceGenerator(name = "seq_nota_fiscal_compra", sequenceName = "seq_nota_fiscal_compra", allocationSize = 1, initialValue = 1)
+@Getter
+@Setter
+public class NotaFiscalCompra implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
+    private Long id;
+
+    private String numeroNotaFiscal;
+    private String SerieNotaFiscal;
+    private String descricaoObs;
+    private BigDecimal valorTotal;
+    private BigDecimal valorDesconto;
+    private BigDecimal valorIcms;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataCompra;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false,
+    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_pagar_id", nullable = false,
+    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_fk"))
+    private ContaPagar contaPagar;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        NotaFiscalCompra that = (NotaFiscalCompra) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
