189b897aa827e6105030d532369ef0be84b60270
dalleth-martinss <essi_dalleth@outlook.com>
2025-01-03 20:14:51 -0300

    create table status-rastreio

---
+++ loja-virtual/src/main/java/br/com/lojavirtual/model/StatusRastreio.java
@@ -0,0 +1,40 @@
+package br.com.lojavirtual.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "status_rastreio")
+@SequenceGenerator(name = "seq_status_rastreio", sequenceName = "seq_status_rastreio", allocationSize = 1, initialValue = 1)
+@Getter
+@Setter
+ public class StatusRastreio implements Serializable {
+   @Serial
+   private static final long serialVersionUID = 1L;
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_status_rastreio")
+   private Long id;
+   private String centroDistribuicao;
+   private String cidade;
+   private String estado;
+   private String status;
+
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    StatusRastreio that = (StatusRastreio) o;
+    return Objects.equals(id, that.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(id);
+  }
+}
