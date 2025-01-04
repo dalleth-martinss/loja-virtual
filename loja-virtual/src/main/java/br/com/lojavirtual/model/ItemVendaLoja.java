fac2d4249b0146baf4307d3cd55f3ecf67fb8f5e
dalleth-martinss <essi_dalleth@outlook.com>
2025-01-03 22:23:16 -0300

    create table item-venda-loja

---
+++ loja-virtual/src/main/java/br/com/lojavirtual/model/ItemVendaLoja.java
@@ -0,0 +1,49 @@
+package br.com.lojavirtual.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "item_venda_loja")
+@SequenceGenerator(name = "seq_item_venda_loja", sequenceName = "seq_item_venda_loja", allocationSize = 1, initialValue = 1)
+@Getter
+@Setter
+ public class ItemVendaLoja implements Serializable {
+   @Serial
+   private static final long serialVersionUID = 1L;
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_item_venda_loja")
+   private Long id;
+
+    private Double quantidade;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id", nullable = false,
+    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Produto produto;
+
+    @ManyToOne
+    @JoinColumn(name = "venda_prod_loja_virt_id", nullable = false,
+    foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "venda_prod_loja_virt_fk"))
+    private VendaProdutoLojaVirtual vendaProdutoLojaVirtual;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ItemVendaLoja that = (ItemVendaLoja) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
