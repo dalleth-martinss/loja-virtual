f234553750e777601817cb8f109ae247758ee258
dalleth-martinss <essi_dalleth@outlook.com>
2025-01-10 17:01:57 -0300

    changes in the table conta-receber

--- loja-virtual/src/main/java/br/com/lojavirtual/model/ContaReceber.java
+++ loja-virtual/src/main/java/br/com/lojavirtual/model/ContaReceber.java
@@ -25,16 +25,21 @@ import java.util.Objects;
    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_receber")
    private Long id;
 
+   @Column(nullable = false)
    private String descricao ;
 
+    @Column(nullable = false)
    @Enumerated(EnumType.STRING)
    private StatusContaReceber status;
 
+   @Column(nullable = false)
    @Temporal(TemporalType.DATE)
    private Date dataVencimento;
 
    @Temporal(TemporalType.DATE)
    private String dataPagamento ;
+
+    @Column(nullable = false)
    private BigDecimal valorTotal;
    private BigDecimal valorDesconto;
 
