23e60e5cf5149c1dc75a4e19ef9873fe11039152
dalleth-martinss <essi_dalleth@outlook.com>
2025-01-13 22:11:43 -0300

    change table produto

--- loja-virtual/src/main/java/br/com/lojavirtual/model/Produto.java
+++ loja-virtual/src/main/java/br/com/lojavirtual/model/Produto.java
@@ -24,25 +24,44 @@ public class Produto implements Serializable {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_produto")
     private Long id;
 
+    @Column(nullable = false)
     private String tipoUnidade;
+
+    @Column(nullable = false)
     private String nome;
+
+    @Column(nullable = false)
     private Boolean ativo = Boolean.TRUE;
 
-    @Column(columnDefinition = "text", length = 2000)
+
+    @Column(columnDefinition = "text", length = 2000 , nullable = false)
     private String descricao;
 
 
   //  private NotaItemProduto notaItemProduto; ASSOCIAR
 
+    @Column(nullable = false)
     private Double peso;
+
+    @Column(nullable = false)
     private Double largura;
+
+    @Column(nullable = false)
     private Double altura;
+
+    @Column(nullable = false)
     private Double profundidade;
 
+    @Column(nullable = false)
     private BigDecimal valorVenda;
+
+    @Column(nullable = false)
     private Integer qtdEstoque;
+
     private Integer qtdAlertaEstoque;
+
     private String linkYouTube;
+
     private Boolean alertaQtdEstoque = Boolean.FALSE;
     private Integer qtdClique = 0;
 
